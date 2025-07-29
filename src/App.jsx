2737c652c52ee6de778608e1cf86a9bb3de570a2
jay2248-yader <jjay55675658@gmail.com>
2025-07-29 16:39:19 +0700

    f2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {  HashRouter as Router, Routes, Route } from "react-router-dom";
+import {  BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TopicL from "./components/TopicL";
 import MemberInput from "./components/MemberInput";
 import Login from "./components/Login";
